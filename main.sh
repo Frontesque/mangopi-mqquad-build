43bd1d1e4f3ef5920989e678cb4718d5feb06cec
Kenny <27463495+Frontesque@users.noreply.github.com>
2024-01-29 21:04:28 -0500

    working ATF

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-working_directory=. # directory for builds to be stored
-source scripts/sunxi-tools.sh #working
-# source scripts/arm-trusted-firmware.sh #broken
+working_directory=$PWD # directory for builds to be stored, '$PWD' is the current directory.
+
+###   Run SunXi Tools Script   ###
+cd $working_directory
+source scripts/sunxi-tools.sh
+
+###   Run Arm Trusted Firmware Script   ###
+cd $working_directory
+source scripts/arm-trusted-firmware.sh
+
+###   Run uBoot Script   ###
+#cd $working_directory
 # source scripts/u-boot.sh #incomplete
--- scripts/arm-trusted-firmware.sh
+++ scripts/arm-trusted-firmware.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt install -y libhdf5-dev
 git clone https://github.com/ARM-software/arm-trusted-firmware.git $working_directory/arm-trusted-firmware
 cd $working_directory/arm-trusted-firmware
 make CROSS_COMPILE=aarch64-linux-gnu PLAT=sun50i_h616 DEBUG=1 bl31
